a4927c976ecd4b3932df1e26a27c2301c86be8af
Brandon Temba <brandontemba560@gmail.com>
2022-06-15 21:31:58 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code goes here
+document.addEventListener( "DOMContentLoaded", function () {
+
+    updateDOM()
+
+  } );
+  
+  
+  function updateDOM() {
+
+    document.getElementById( "text" )
+
+      .innerHTML = "This is really cool!";
+      
+  }
